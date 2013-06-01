5912ba39d26ce25f68682c90b84be354f2cea6a3
Lauren Roth <laurenroth@gmail.com>
2013-06-01 15:24:05 -0700

    initial commit for Hairology

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hairology::Application.config.secret_token = '54b05dfee639e2d98aa96b9948f550f0cfbdeb9b5a8491662cfeff1c853c2130a445fa28cc52622954ee0c944f95c40dfd3b333f7d95613062de9c79bd11a395'
