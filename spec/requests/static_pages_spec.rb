636087e596188e7e706dd9380b594f4a69a055a3
Lauren Roth <laurenroth@gmail.com>
2013-06-01 16:51:50 -0700

    add static pages with dynamic titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,22 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Hairology')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Home" )
+    end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       page.should have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Help" )
     end
   end
 
@@ -21,12 +31,22 @@ describe "Static pages" do
       visit '/static_pages/explore'
       page.should have_content('Explore')
     end
+
+    it "should have the title 'Explore'" do
+      visit '/static_pages/explore'
+      page.should have_selector('title', :text => "Explore" )
+    end
   end
 
   describe "Profile page" do
     it "should have the content 'Profile'" do
       visit '/static_pages/profile'
       page.should have_content('Profile')
+    end
+
+    it "should have the title 'Profile'" do
+      visit '/static_pages/profile'
+      page.should have_selector('title', :text => "Profile" )
     end
   end
 end
